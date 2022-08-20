269d6ac05d556c5af798c74729c78b8f6340a701
datng87 <dat.q.nguyen87@gmail.com>
2022-08-21 03:15:26 +1000

    bonus done

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,9 +1,1 @@
-//plot gauge ids[0]
-
-var gaugedata={};
-metadata.forEach(function(element){
-    if (element.id==ids[0]){
-        gaugedata = element;
-    };
-});
 //code for gauge chart in app.js
